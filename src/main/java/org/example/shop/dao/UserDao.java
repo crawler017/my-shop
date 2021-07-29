1edd67b2c9dfa266128370ed694424dd1a36f014
crawler017 <gazer1406819367@gmail.com>
2021-07-29 10:21:43 +0800

    update structure

--- src/main/java/org/example/shop/dao/UserDao.java
+++ src/main/java/org/example/shop/dao/UserDao.java
@@ -1,4 +1,14 @@
 package org.example.shop.dao;
 
+import org.example.shop.entity.User;
+
 public interface UserDao {
+
+    /**
+     * get User by email and password
+     * @param email
+     * @param password
+     * @return User
+     */
+    public User getUser(String email, String password);
 }
---
+++ src/main/java/org/example/shop/dao/impl/UserDaoImpl.java
@@ -0,0 +1,11 @@
+package org.example.shop.dao.impl;
+
+import org.example.shop.dao.UserDao;
+import org.example.shop.entity.User;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public User getUser(String email, String password) {
+        return null;
+    }
+}
