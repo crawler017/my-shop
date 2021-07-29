e881e29defddb8d0aad548c98198dd4fb78c0538
crawler017 <gazer1406819367@gmail.com>
2021-07-30 07:15:33 +0800

    update structure

--- src/main/java/org/example/shop/web/Controller/LoginController.java
+++ src/main/java/org/example/shop/web/Controller/LoginController.java
@@ -4,19 +4,17 @@ import org.example.shop.dao.impl.UserDaoImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class LoginController extends HttpServlet {
     private static final Logger logger = LoggerFactory.getLogger(UserDaoImpl.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        String email = req.getParameter("email");
-        String password = req.getParameter("password");
-
-        logger.info("user \"{}\" 's password: {}", email, password);
 
     }
 
@@ -24,6 +22,13 @@ public class LoginController extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
         String email = req.getParameter("email");
         String password = req.getParameter("password");
+
+        logger.info("user \"{}\" 's password: {}", email, password);
+        try {
+            req.getRequestDispatcher("login.html").forward(req, resp);
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
