036150d9352c3c1d6f220ffebf778831df181014
crawler017 <gazer1406819367@gmail.com>
2021-08-03 18:17:55 +0800

    update structure

---
+++ src/main/java/org/example/shop/web/interceptor/LoginInterceptor.java
@@ -0,0 +1,29 @@
+package org.example.shop.web.interceptor;
+
+import org.example.shop.entity.User;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        User user = (User) httpServletRequest.getSession().getAttribute("user");
+        if (null == user) {
+            httpServletResponse.sendRedirect("login");
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        System.out.println("====" + modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
